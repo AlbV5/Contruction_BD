e29e4215dd7a6eca91648df63962add0836bfac2
AlbV5 <119131365+AlbV5@users.noreply.github.com>
2023-03-23 21:44:00 -0600

    Add files via upload

--- ejemplo405.sql
+++ ejemplo405.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-03-2023 a las 15:31:07
--- Versión del servidor: 10.4.11-MariaDB
--- Versión de PHP: 7.4.1
+-- Tiempo de generación: 24-03-2023 a las 04:42:35
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -25,6 +24,34 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `plantel`
+--
+
+CREATE TABLE `plantel` (
+  `Id_Plantel` int(11) NOT NULL,
+  `NombrePlantel` varchar(55) NOT NULL,
+  `Clave` varchar(55) NOT NULL,
+  `Direccion` text NOT NULL,
+  `TelefonoPlantel` varchar(15) NOT NULL,
+  `EmailPlantel` varchar(255) NOT NULL,
+  `IdMunicipio` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipousuarios`
+--
+
+CREATE TABLE `tipousuarios` (
+  `Id_TUsuario` int(11) NOT NULL,
+  `NombreTUsuario` varchar(55) NOT NULL,
+  `Descripcion` varchar(55) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuarios`
 --
 
@@ -36,12 +63,25 @@ CREATE TABLE `usuarios` (
   `Telefono` varchar(55) NOT NULL,
   `Email` varchar(255) NOT NULL,
   `Password` varchar(55) NOT NULL,
-  `Id_Tusuario` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `Id_Tusuario` int(11) NOT NULL,
+  `IdPlantel` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `plantel`
+--
+ALTER TABLE `plantel`
+  ADD PRIMARY KEY (`Id_Plantel`);
+
+--
+-- Indices de la tabla `tipousuarios`
+--
+ALTER TABLE `tipousuarios`
+  ADD PRIMARY KEY (`Id_TUsuario`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -54,10 +94,16 @@ ALTER TABLE `usuarios`
 --
 
 --
--- AUTO_INCREMENT de la tabla `usuarios`
+-- AUTO_INCREMENT de la tabla `plantel`
 --
-ALTER TABLE `usuarios`
-  MODIFY `Id_Usuario` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `plantel`
+  MODIFY `Id_Plantel` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tipousuarios`
+--
+ALTER TABLE `tipousuarios`
+  MODIFY `Id_TUsuario` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
